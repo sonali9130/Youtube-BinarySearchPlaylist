b9e8a29934e0ef34a0d38347fe8da09be52ae183
sonali9130 <sonaligupta9130@gmail.com>
2022-08-14 16:14:49 +0530

    Create homeworkproblem1.cpp
    https://youtu.be/cQ5I5eoVQlQ

---
+++ homeworkproblem1.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int search(const ArrayReader& reader, int target) {
+        int lo=0, hi=1;
+        while(1){
+            int val=reader.get(hi);
+            if(val==target){
+                return hi;
+            }
+            else if(val> target){
+                hi--;
+                break;
+            }
+            else{
+                lo=hi+1;
+                hi<<=1;//hi*=2
+            }
+        }
+        
+        while(lo<=hi){
+            int mid=lo+((hi-lo)>>1);
+            int val=reader.get(mid);
+            if(val==target){
+                return mid;
+            }
+            else if(val>target){
+                hi=mid-1;
+            }
+            else{
+                lo=mid+1;
+            }
+        }
+        return -1;
+    }
+};
