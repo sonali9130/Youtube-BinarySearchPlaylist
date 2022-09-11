222a40d4421b7c97fe5b8afa60a1c14722ea9434
sonali9130 <sonaligupta9130@gmail.com>
2022-09-11 15:51:31 +0530

    Update leetcode33.cpp

--- leetcode33.cpp
+++ leetcode33.cpp
@@ -1,1 +1,29 @@
-
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int lo=0, hi=nums.size()-1;       
+        while(lo<=hi){
+            int mid=lo+((hi-lo)>>1);
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]>=nums[lo]){
+                if(nums[lo]<=target && nums[mid]>target){
+                    hi=mid-1;
+                }
+                else{
+                    lo=mid+1;
+                }
+            }
+            else{
+                if(nums[hi]>=target && nums[mid]<target){
+                    lo=mid+1;
+                }
+                else{
+                    hi=mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+};
