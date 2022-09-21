d99aa55c2d7b31cabc06e7b9d1bdfe6b39455915
sonali9130 <sonaligupta9130@gmail.com>
2022-09-21 23:16:35 +0530

    Update leetcode69.cpp

--- leetcode69.cpp
+++ leetcode69.cpp
@@ -1,1 +1,27 @@
-
+class Solution {
+public:
+    int mySqrt(int num) {
+        if(num==0){
+            return 0;
+        }
+        else if(num<4){
+            return 1;
+        }
+        int lo=2, hi=num/2;
+        while(lo<=hi){
+            int mid=lo+((hi-lo)>>1);
+            long long prod=mid*(long long)mid;
+            
+            if(prod==num){
+                return mid;
+            }
+            else if(prod>num){
+                hi=mid-1;
+            }
+            else{
+                lo=mid+1;
+            }
+        }
+        return hi;
+    }
+};
