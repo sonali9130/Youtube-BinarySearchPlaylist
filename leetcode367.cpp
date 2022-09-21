3fa0e2e35bb3f71f7070a0fbd9a730ca88e6573d
sonali9130 <sonaligupta9130@gmail.com>
2022-09-21 23:12:54 +0530

    Update leetcode367.cpp

--- leetcode367.cpp
+++ leetcode367.cpp
@@ -1,1 +1,29 @@
-
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        if(num==1){
+            return true;
+        }
+        else if(num<4){
+            return false;
+        }
+        
+        int lo=2, hi=(num>>1);
+        
+        while(lo<=hi){
+            int mid=lo+((hi-lo)>>1);
+            long long prod=mid*(long long)mid;
+            
+            if(prod==num){
+                return true;
+            }
+            else if(prod>num){
+                hi=mid-1;
+            }
+            else{
+                lo=mid+1;
+            }
+        }
+        return false;
+    }
+};
